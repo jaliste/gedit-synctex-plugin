fb08b74bafc66210dbba7939d943049c6c4848fb
Jose Aliste <jaliste@src.gnome.org>
2010-08-06 23:36:38 -0400

    Adapt evince_dbus.py to act as a script that supports forward search.

--- synctex/evince_dbus.py
+++ synctex/evince_dbus.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 # This file is part of the Gedit Synctex plugin.
@@ -19,6 +20,7 @@
 # Street, Fifth Floor, Boston, MA  02110-1301, USA
 
 import dbus, subprocess, time
+from traceback import print_exc
 
 RUNNING, CLOSED = range(2)
 
@@ -27,42 +29,31 @@ class EvinceController:
 	daemon = None
 
 	def __init__(self, uri, spawn = False):
-#		self.context = context
 		self.uri = uri
 		self.spawn = spawn
 		self.status = CLOSED
 		bus = dbus.SessionBus()
-		if spawn:
-			self.spawn_evince()
-		self.get_dbus_name()
+		self._get_dbus_name()
 	
-	def get_dbus_name (self):
+	def _get_dbus_name (self):
 		try:    			
 			if EvinceController.daemon is None:
-				print "Conectandose al Evince Daemon"
 				EvinceController.daemon = bus.get_object('org.gnome.evince.Daemon','/org/gnome/evince/Daemon')
-			self.dbus_name = EvinceController.daemon.FindDocument(self.uri)
+			self.dbus_name = EvinceController.daemon.FindDocument(self.uri, self.spawn, dbus_interface = "org.gnome.evince.Daemon")
 			if self.dbus_name != '':
 				self.status = RUNNING
-				print self.dbus_name
-				self.window = bus.get_object(self.dbus_name, '/org/gnome/evinve/Application')
+				self.window = bus.get_object(self.dbus_name, '/org/gnome/evince/Application')
 				self.window = bus.get_object(self.dbus_name, '/org/gnome/evince/Window/0')
 				self.window.connect_to_signal("SyncSource", self.sync_source_cb, dbus_interface="org.gnome.evince.Window")
 				self.window.connect_to_signal("Closed", self.on_window_close, dbus_interface="org.gnome.evince.Window")
 		except dbus.DBusException:
-			traceback.print_exc()
+			print_exc()		
 		bus.add_signal_receiver(self.name_owner_changed_cb, signal_name = "NameOwnerChanged")
 	def on_window_close(self):
-		print "Window closed", self.window
-
-	def spawn_evince(self):
-		self.proc = subprocess.Popen(['/home/jaliste/dev/evince/shell/evince', self.uri])
-		# THE FOLLOWING IS WRONG!
-		time.sleep(0.3)
+		self.status = CLOSED
 
 	def name_owner_changed_cb(self, service_name, old_owner, new_owner):
 		if service_name == self.dbus_name and new_owner == '': 
-			print "Evince was closed. spawn = ", self.spawn
 			self.status = CLOSED
 	def sync_source_cb(self, source_file, source_point):
 		"""
@@ -71,8 +62,6 @@ class EvinceController:
 		@param filename: the filename
 		@param line: a line number counting from 1 (!)
 		"""
-		#_log.debug("Received inverse DVI search call: %s %s" % (filename, line))
-		
 		print source_file, source_point
 		#file = File(filename)
 		
@@ -87,26 +76,45 @@ class EvinceController:
 		#except KeyError:
 		#	raise("Could not activate tab for file %s" % filename)
 			
-	def SyncView(self, data):
+	def SyncView(self, input_file, data):
+		print self.status
 		if self.status == CLOSED:
 			if not self.spawn:
-				return
-			self.spawn_evince()
-			self.get_dbus_name()
-		self.window.SyncView('/home/jaliste/Documents/research/articles/rapid_convergence/10.tex',
-		[data, 1],dbus_interface="org.gnome.evince.Window")
+				self._get_dbus_name()
+		# if self.status is still closed, it means there is a 
+		# problem running evince.
+		if self.status == CLOSED: 
+			return False
+		print input_file, data
+		self.window.SyncView(input_file, (data, 1), dbus_interface="org.gnome.evince.Window")
 		return False				
-
+def print_usage():
+	print '''
+The usage is evince_dbus output_file line_number input_file from the directory of output_file. 
+'''
+## This file can be used as a script to support forward search and backward search in vim.
+## It should be easy to adapt to other editors. 
+##  evince_dbus  pdf_file  line_source input_file
 if __name__ == '__main__':
-	import dbus.mainloop.glib, gobject, glib
+	import dbus.mainloop.glib, gobject, glib, sys, os
+	if len(sys.argv)!=4:
+		print_usage()
+		sys.exit(1)	
+	try:
+		line_number = int(sys.argv[2])
+	except ValueError:
+		print_usage()
+		sys.exit(1)
+	
+	output_file = sys.argv[1]
+	input_file  = sys.argv[3]
+        path_output  = os.getcwd() + '/' + output_file
+	path_input   = os.getcwd() + '/' + input_file
+
 	dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 	bus = dbus.SessionBus()
 
-
- 	a = EvinceController('file:///home/jaliste/Documents/research/articles/rapid_convergence/10.pdf', True)
-	glib.timeout_add(10000, a.SyncView, 300)
+ 	a = EvinceController('file://' + path_output, True )
+        glib.timeout_add(1000, a.SyncView, path_input, line_number)
 	loop = gobject.MainLoop()
-	
-	loop.run()  
-
-
+	loop.run() 
