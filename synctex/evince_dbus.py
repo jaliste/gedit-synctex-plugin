b79027499109b8aadaa923c176079285f162da49
Jose Aliste <jaliste@src.gnome.org>
2010-08-07 23:02:33 -0400

    Add ForwardSearch support in the plugin.

--- synctex/evince_dbus.py
+++ synctex/evince_dbus.py
@@ -24,97 +24,91 @@ from traceback import print_exc
 
 RUNNING, CLOSED = range(2)
 
-class EvinceController:
-	"""A custom object to control an Evince process through DBUS."""
+class EvinceWindowProxy:
+	"""A DBUS proxy for an Evince Window."""
 	daemon = None
-
+	sync_source_handler = None
+	bus = None
+	
 	def __init__(self, uri, spawn = False):
 		self.uri = uri
 		self.spawn = spawn
 		self.status = CLOSED
-		bus = dbus.SessionBus()
-		self._get_dbus_name()
-	
-	def _get_dbus_name (self):
-		try:    			
-			if EvinceController.daemon is None:
-				EvinceController.daemon = bus.get_object('org.gnome.evince.Daemon','/org/gnome/evince/Daemon')
-			self.dbus_name = EvinceController.daemon.FindDocument(self.uri, self.spawn, dbus_interface = "org.gnome.evince.Daemon")
+		try: 
+			if EvinceWindowProxy.bus is None:
+				EvinceWindowProxy.bus = dbus.SessionBus()
+			if EvinceWindowProxy.daemon is None:
+				EvinceWindowProxy.daemon = EvinceWindowProxy.bus.get_object('org.gnome.evince.Daemon',
+							   '/org/gnome/evince/Daemon')
+			self._get_dbus_name(False)
+		except dbus.DBusException:
+			print_exc()
+
+	def _get_dbus_name(self, spawn):
+		try:
+			self.dbus_name = self.daemon.FindDocument(self.uri,spawn, dbus_interface = "org.gnome.evince.Daemon")
 			if self.dbus_name != '':
 				self.status = RUNNING
-				self.window = bus.get_object(self.dbus_name, '/org/gnome/evince/Application')
-				self.window = bus.get_object(self.dbus_name, '/org/gnome/evince/Window/0')
-				self.window.connect_to_signal("SyncSource", self.sync_source_cb, dbus_interface="org.gnome.evince.Window")
-				self.window.connect_to_signal("Closed", self.on_window_close, dbus_interface="org.gnome.evince.Window")
+				#self.window = EvinceWindowProxy.bus.get_object(self.dbus_name, '/org/gnome/evince/Application')
+				self.window = EvinceWindowProxy.bus.get_object(self.dbus_name, '/org/gnome/evince/Window/0')
+				#self.window.connect_to_signal("Closed", self.on_window_close, dbus_interface="org.gnome.evince.Window")
 		except dbus.DBusException:
 			print_exc()		
-		bus.add_signal_receiver(self.name_owner_changed_cb, signal_name = "NameOwnerChanged")
+		#bus.add_signal_receiver(self.name_owner_changed_cb, signal_name = "NameOwnerChanged")
+	
 	def on_window_close(self):
 		self.status = CLOSED
+		self.window = None
 
+	
 	def name_owner_changed_cb(self, service_name, old_owner, new_owner):
 		if service_name == self.dbus_name and new_owner == '': 
 			self.status = CLOSED
-	def sync_source_cb(self, source_file, source_point):
-		"""
-		A service call has been received
-		
-		@param filename: the filename
-		@param line: a line number counting from 1 (!)
-		"""
-		print source_file, source_point
-		#file = File(filename)
-		
-		#try:
-		#	%self._context.activate_editor(file)
-		#	editor = self._context.active_editor
-			
-		#	assert type(editor) is LaTeXEditor
-			
-		#	editor.select_lines(line - 1)
-			
-		#except KeyError:
-		#	raise("Could not activate tab for file %s" % filename)
-			
+	
 	def SyncView(self, input_file, data):
-		print self.status
 		if self.status == CLOSED:
-			if not self.spawn:
-				self._get_dbus_name()
+			print "Evince is closed"
+			if self.spawn:
+				print "Spawning evince"
+				self._get_dbus_name(True)
 		# if self.status is still closed, it means there is a 
 		# problem running evince.
 		if self.status == CLOSED: 
 			return False
-		print input_file, data
-		self.window.SyncView(input_file, (data, 1), dbus_interface="org.gnome.evince.Window")
+		self.window.SyncView(input_file, data, dbus_interface="org.gnome.evince.Window")
 		return False				
-def print_usage():
-	print '''
-The usage is evince_dbus output_file line_number input_file from the directory of output_file. 
-'''
 ## This file can be used as a script to support forward search and backward search in vim.
 ## It should be easy to adapt to other editors. 
 ##  evince_dbus  pdf_file  line_source input_file
 if __name__ == '__main__':
 	import dbus.mainloop.glib, gobject, glib, sys, os
+	
+	def print_usage():
+	        print '''
+The usage is evince_dbus output_file line_number input_file from the directory of output_file. 
+'''
+		sys.exit(1)
+
+	
 	if len(sys.argv)!=4:
 		print_usage()
-		sys.exit(1)	
 	try:
 		line_number = int(sys.argv[2])
 	except ValueError:
 		print_usage()
-		sys.exit(1)
 	
 	output_file = sys.argv[1]
 	input_file  = sys.argv[3]
         path_output  = os.getcwd() + '/' + output_file
 	path_input   = os.getcwd() + '/' + input_file
 
+	if not os.path.isfile(path_output):
+		print_usage()
+
 	dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 	bus = dbus.SessionBus()
 
- 	a = EvinceController('file://' + path_output, True )
-        glib.timeout_add(1000, a.SyncView, path_input, line_number)
+ 	a = EvinceWindowProxy(bus, 'file://' + path_output, True )
+        glib.timeout_add(1000, a.SyncView, path_input, (line_number,1))
 	loop = gobject.MainLoop()
 	loop.run() 
--- synctex/synctex.py
+++ synctex/synctex.py
@@ -22,7 +22,7 @@
 import gtk
 import gedit
 from gettext import gettext as _
-from evince_dbus import EvinceController
+from evince_dbus import EvinceWindowProxy
 
 ui_str = """<ui>
   <menubar name="MenuBar">
@@ -35,19 +35,73 @@ ui_str = """<ui>
 </ui>
 """
 
+VIEW_DATA_KEY = "SynctexPluginViewData"
+
+
+class SynctexViewHelper:
+
+    mime_types = ['text/x-tex'];
+
+    def __init__(self, view):
+        self._view = view
+        self._doc = view.get_buffer()
+        self.window_proxy = None
+        self._handlers = [
+            self._doc.connect('saved', self.on_saved_or_loaded),
+            self._doc.connect('loaded', self.on_saved_or_loaded)
+        ]
+        self.active = False
+        self.update_uri_mime_type()
+
+    def on_saved_or_loaded(self, doc, data):
+        self.update_uri_mime_type()
+    
+    def deactivate(self):
+        pass
+
+    def update_uri_mime_type(self):
+        self.uri = self._doc.get_uri()
+        self.mime_type = self._doc.get_mime_type()
+        self.update_active()
+   
+    def update_active(self):
+        # Activate the plugin only if the doc is a LaTeX file. 
+        self.active = (self.mime_type in self.mime_types)
+
+        if self.active and self.window_proxy is None:
+            if self.uri.endswith(".tex"):
+                uri_output = self.uri[:-3] + "pdf"
+                self.window_proxy = EvinceWindowProxy (uri_output, True)
+                print "activando"
+        elif not self.active and self.window_proxy is not None:
+            # destroy the evince window proxy.
+            print "desactivando"
+            pass
+
 class SynctexWindowHelper:
     def __init__(self, plugin, window):
         self._window = window
         self._plugin = plugin
         self._insert_menu()
-        self._window.connect("tab_added", self._on_tab_added),
-        self._window.connect("tab_removed", self._on_tab_removed),
-        self._window.connect("active_tab_changed", self._on_active_tab_changed),
-	self.active_document = None
-	self.active_uri = None
-	self.evince_controller = EvinceController() 
+
+        for view in window.get_views():
+            self.add_helper(view)
+
+        self.handlers = [
+            window.connect("tab-added", lambda w, t: self.add_helper(t.get_view())),
+            window.connect("tab-removed", lambda w, t: self.remove_helper(t.get_view()))
+        ]
+        #window.set_data(self.WINDOW_DATA_KEY, (added_hid, removed_hid))
+
         #self._window.connect("destroy", self._on_window_destroyed) ]
 
+    def add_helper(self, view):
+        helper = SynctexViewHelper(view)
+        view.set_data (VIEW_DATA_KEY, helper)
+
+    def remove_helper(self, view):
+        view.get_data(VIEW_DATA_KEY).deactivate()
+        view.set_data(VIEW_DATA_KEY, None)
 
     def __del__(self):
         self._window = None
@@ -72,37 +126,17 @@ class SynctexWindowHelper:
         # Merge the UI
         self._ui_id = manager.add_ui_from_string(ui_str)
 
-        
-
-    def update_ui(self):
-	new_doc = self._window.get_active_document()
- 	new_uri = new_doc.get_uri()
-        if self.active_document == new_doc and self.active_uri == new_uri:
-             return
-	self.active_document = new_doc
-	self.active_uri = new_uri
-        print "UPDATE UI", self._window.get_active_document()
-        pass
-
     def forward_search_cb(self, action):
-        doc = self._window.get_active_document()
-
-        if doc.get_mime_type()=='text/x-tex':
+        view = self._window.get_active_view()
+        view_helper  = view.get_data(VIEW_DATA_KEY)
+        doc = view.get_buffer()
+     
+        if view_helper.active:
             cursor_iter =  doc.get_iter_at_mark(doc.get_insert())
             line = cursor_iter.get_line()
             col = cursor_iter.get_line_offset()
-            evince_controller.forward_search(doc.get_uri(), line, col)
-        if not doc:
-            return
-
-    def _on_tab_added(self, window, tab):
-        print "Tab added ", tab.get_document().get_uri()
-        pass
-    def _on_tab_removed(self, window, tab):
-        pass
-    def _on_active_tab_changed(self, window, tab):
-        print "Active tab changed ", tab.get_document().get_mime_type()
-        pass
+            print "syncview", view_helper.uri[7:], (line, col)
+            view_helper.window_proxy.SyncView(view_helper.uri[7:], (line, col))
 
 class SynctexPlugin(gedit.Plugin):
     WINDOW_DATA_KEY = "SynctexPluginWindowData"
@@ -119,7 +153,8 @@ class SynctexPlugin(gedit.Plugin):
         window.set_data(self.WINDOW_DATA_KEY, None)
         
     def update_ui(self, window):
-        window.get_data(self.WINDOW_DATA_KEY).update_ui()
+        pass
+#        window.get_data(self.WINDOW_DATA_KEY).update_ui()
 
 
 # ex:ts=4:et:
